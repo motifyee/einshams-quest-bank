43d9faba116f5163ebc7401b0128f9d6ae4ca068
MohamedRT <motifyee@gmail.com>
2023-05-14 15:14:28 +0300

    added a note for using github with ssh @main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -21,3 +21,30 @@ ReactDOM.createRoot(root).render(
     </QuestionsProvider>
     // </React.StrictMode>,
 );
+
+/**
+ git remote set-url origin git@motifyee:motifyee/einshams-quest-bank
+    
+
+    * ssh-keygen -t ed25519 -C "motifyee@gmail.com"
+    * ssh-keygen -t ed25519 -C "mrt.tarabia@gmail.com"
+
+    * ssh-add ~/.ssh/motifyee-github
+    * ssh-add ~/.ssh/mrt-web-github
+    
+    * ssh-add -l # to list identities
+    * ssh-add -D # to remove all identities
+
+ 
+    *  ~/.ssh/config:
+
+        Host git.motifyee
+            HostName github.com
+            IdentityFile ~/.ssh/motifyee-github
+            IdentitiesOnly yes
+        Host mrt-web
+            HostName github.com
+            IdentityFile ~/.ssh/mrt-web-github
+            IdentitiesOnly yes
+
+ *  */
