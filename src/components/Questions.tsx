80f1932c6f135340570d6073b875b8f0fab1d156
MohamedRT <motifyee@gmail.com>
2023-05-22 23:10:39 +0300

    fix scroll-top raising and hiding effect

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -54,11 +54,16 @@ function ScrollTopButton({
 }: {
     questPanel: React.RefObject<HTMLDivElement>;
 }) {
+    const settings = useContext(SettingsContext);
+
+    const raise = settings.testModeOn && settings.correctAnswers ? 'raise' : '',
+        show = !settings.sidebarOn ? '' : 'none';
+
     const [hover, setHover] = useState('');
 
     return (
         <div
-            className={`scroll-top ${hover}`}
+            className={`scroll-top ${raise} ${show} ${hover}`}
             onClick={() =>
                 questPanel.current?.scrollTo({
                     top: 0,
